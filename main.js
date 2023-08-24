52737785ccd2f17b55480195b3de7ee3077ce403
Hook03cdl <carlosduverney@gmail.com>
2023-08-23 19:43:29 -0700

    commit

--- main.js
+++ main.js
@@ -6,14 +6,34 @@ const ancho = window.innerWidth - 200;
 micanvas.height = altura;
 micanvas.width = ancho;
 const ctx = micanvas.getContext('2d');
+var x = ancho / 2 - 25;
+var y = altura / 2 - 25;
+ctx.strokeRect(x, y, 100, 100, 10);
+colorRandom();
+ctx.fillRect(x, y, 100, 100);
 
-window.addEventListener('keydown', () => {
-	pintarRandom();
+window.addEventListener('keydown', (event) => {
+	switch (event.keyCode) {
+		case 87:
+			y -= 30;
+			pintarRandom();
+			break;
+		case 83:
+			y += 30;
+			pintarRandom();
+			break;
+		case 68:
+			x += 30;
+			pintarRandom();
+			break;
+		case 65:
+			x -= 30;
+			pintarRandom();
+			break;
+	}
 });
 
 function pintarRandom() {
-	let x = Math.floor(Math.random() * ancho - 100);
-	let y = Math.floor(Math.random() * altura - 100);
 	ctx.strokeRect(x, y, 100, 100, 10);
 	colorRandom();
 	ctx.fillRect(x, y, 100, 100);
